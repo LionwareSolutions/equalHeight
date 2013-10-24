c6591b02656bb039ee6895dc92fb89e4d26f1eb8
Nick Stemerdink <nick.stemerdink@stemerdink-it.com>
2013-10-24 11:35:01 +0200

    Initial checkin

---
+++ equalHeight.js
@@ -0,0 +1,34 @@
+/*
+ * jQuery Equal Height Plugin
+ *
+ * Copyright (c) 2013 Stemerdink IT
+ * Dual licensed under the MIT and GPL licenses
+ */
+(function($) {
+    jQuery.fn.equalizeHeight = function(fillElementSelector, heightCalculationSelector) {
+        if (typeof heightCalculationSelector == "undefined") {
+            heightCalculationSelector = fillElementSelector;
+        }
+        this.each(function() {
+            var container = jQuery(this);
+
+            var highest = 0;
+            container.find(heightCalculationSelector).each(function(){
+                var height = jQuery(this).height();
+                if (height > highest) {
+                    highest = height;
+                }
+            });
+
+            var scalableElements = container.find(fillElementSelector);
+            scalableElements.each(function(){
+                var actualElementHeight = scalableElements.height();
+                var heightToAdd = highest - actualElementHeight;
+
+                var newHeight = actualElementHeight + heightToAdd;
+
+                scalableElements.height(newHeight);
+            });
+        });
+    };
+})(jQuery);
